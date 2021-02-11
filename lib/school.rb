ca6e49ad508ea7a40a9343b8ac9d86dd17e14809
Jonathan Block <blockjonathan29@gmail.com>
2021-02-11 10:58:49 -0500

    commit tree 99ae7b38928707cd4caafabd110cadb187032bdc

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+    
+    attr_reader :school_name
+    attr_accessor :roster 
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(student_name, student_grade)
+        @roster[student_grade] ||= []
+        @roster[student_grade] << student_name
+    end
+
+    def grade(student_grade)
+        @roster[student_grade]
+    end
+
+    def sort
+      @roster.each do |student_grade, student_name|
+        student_name.sort!
+      end
+      @roster.sort.map{|student_grade, student_name| [student_grade, student_name]}.to_h
+    end
+
+end
